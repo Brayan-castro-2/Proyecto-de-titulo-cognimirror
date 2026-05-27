68ba1a59572ae3d5ab6cf19ffcc45ac027941ca0
Brayan-castro-2 <96713974+Brayan-castro-2@users.noreply.github.com>
2026-05-27 06:07:22 -0400

    fix: usar variables de entorno para Supabase

--- Producto/utils/supabaseClient.js
+++ Producto/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://viqtdxvoryovilzsfhwu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpcXRkeHZvcnlvdmlsenNmaHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MDcxNzQsImV4cCI6MjA5MjQ4MzE3NH0.ZPyzo2vYCjo1Wozy0s3eVkyS4zmeLH3m7zVo4NiSHJ0';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
